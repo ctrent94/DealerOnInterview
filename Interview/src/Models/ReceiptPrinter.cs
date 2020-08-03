bfe02680b69a45cae7a4b8b64910c1c34c2f7b8e
ctrent94 <ctrent94@outlook.com>
2020-08-03 08:47:54 -0700

    Upper case the names

--- Interview/src/Models/ReceiptPrinter.cs
+++ Interview/src/Models/ReceiptPrinter.cs
@@ -30,7 +30,7 @@ namespace Models {
 
                 totalOrderPrice += finalPriceSummation;
                 totalOrderTax += finalTaxSummation;
-                Console.WriteLine ($"Item name: {group.Key}, Total cost: {finalPriceSummation}, {costPerUnitDisplay}");
+                Console.WriteLine ($"Item name: {group.Key.ToUpper()}, Total cost: {finalPriceSummation}, {costPerUnitDisplay}");
             }
             Console.WriteLine($"Sales Taxes: {totalOrderTax}");
             Console.WriteLine($"Total: {totalOrderPrice}");
